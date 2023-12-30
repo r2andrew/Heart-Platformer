43c56683bde08df8c3a0feb502ff42422675032e
r2andrew <abradley4646@gmail.com>
2023-12-30 14:07:41 +0000

    9/15

--- player.gd
+++ player.gd
@@ -31,7 +31,7 @@ func _physics_process(delta):
 	
 	handle_jump()
 
-	var input_axis = Input.get_axis("ui_left", "ui_right")
+	var input_axis = Input.get_axis("move_left", "move_right")
 
 	if input_axis != 0:
 		if is_on_floor():
@@ -59,7 +59,7 @@ func apply_gravity(delta):
 
 func handle_wall_jump():
 	var wall_normal = get_wall_normal()
-	if Input.is_action_just_pressed("ui_up"):
+	if Input.is_action_just_pressed("jump"):
 		velocity.x = wall_normal.x * movement_data.speed
 		velocity.y = movement_data.jump_velocity * 0.8
 		just_wall_jumped = true
@@ -69,12 +69,12 @@ func handle_jump():
 	if is_on_floor(): air_jump = true
 	# Handle jump.
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
-		if Input.is_action_just_pressed("ui_up"):
+		if Input.is_action_just_pressed("jump"):
 			velocity.y = movement_data.jump_velocity
 	elif not is_on_floor():
-		if Input.is_action_just_released("ui_up") and velocity.y < movement_data.jump_velocity / 2:
+		if Input.is_action_just_released("jump") and velocity.y < movement_data.jump_velocity / 2:
 			velocity.y = movement_data.jump_velocity / 2
-		if Input.is_action_just_pressed("ui_up") and air_jump and not just_wall_jumped:
+		if Input.is_action_just_pressed("jump") and air_jump and not just_wall_jumped:
 			velocity.y = movement_data.jump_velocity * 0.8
 			air_jump = false
 
